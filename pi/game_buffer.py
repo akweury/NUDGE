426ba93b44fdea08f0b2c508fe44cf6077b2c872
JST <shajingyuan@me.com>
2024-02-12 22:45:42 +0100

    add game_buffer and game_render

---
+++ pi/game_buffer.py
@@ -0,0 +1,225 @@
+# Created by shaji at 01/01/2024
+import os
+import random
+from tqdm import tqdm
+from OC_Atari.ocatari.core import OCAtari
+from PIL import Image
+
+from pi.utils.EnvArgs import EnvArgs
+from pi.utils import game_utils
+from pi.utils.game_utils import RolloutBuffer
+from pi.utils.oc_utils import extract_logic_state_assault, extract_logic_state_atari
+from src.environments.getout.getout.getout.getout import Getout
+from src.environments.getout.getout.getout.paramLevelGenerator import ParameterizedLevelGenerator
+from src.agents.utils_getout import extract_logic_state_getout
+
+
+def create_getout_instance(args, seed=None):
+    if args.m == 'getoutplus':
+        enemies = True
+    else:
+        enemies = False
+    # level_generator = DummyGenerator()
+    getout = Getout()
+    level_generator = ParameterizedLevelGenerator(enemies=enemies)
+    level_generator.generate(getout, seed=seed)
+    getout.render()
+
+    return getout
+
+
+def collect_data_getout(agent, args):
+    game_num = 300
+    # play games using the random agent
+    seed = random.seed() if args.seed is None else int(args.seed)
+    args.filename = args.m + '_' + args.teacher_agent + '_episode_' + str(game_num) + '.json'
+
+    buffer = RolloutBuffer(args.filename)
+
+    if os.path.exists(buffer.filename):
+        return
+    # collect data
+    step = 0
+    win_count = 0
+    win_rates = []
+    if args.m == 'getout':
+        game_env = create_getout_instance(args)
+
+        # frame rate limiting
+        for i in tqdm(range(game_num), desc=f"win counter: {win_count}"):
+            step += 1
+            logic_states = []
+            actions = []
+            rewards = []
+            frame_counter = 0
+
+            # play a game
+            while not (game_env.level.terminated):
+                # random actions
+                action = agent.reasoning_act(game_env)
+                logic_state = extract_logic_state_getout(game_env, args).squeeze()
+                try:
+                    reward = game_env.step(action)
+                except KeyError:
+                    game_env.level.terminated = True
+                    game_env.level.lost = True
+                    break
+                if frame_counter == 0:
+                    logic_states.append(logic_state.detach().tolist())
+                    actions.append(action - 1)
+                    rewards.append(reward)
+                elif action - 1 != actions[-1] or frame_counter % 5 == 0:
+                    logic_states.append(logic_state.detach().tolist())
+                    actions.append(action - 1)
+                    rewards.append(reward)
+
+                frame_counter += 1
+
+            # save game buffer
+            if not game_env.level.lost:
+                buffer.logic_states.append(logic_states)
+                buffer.actions.append(actions)
+                buffer.rewards.append(rewards)
+                win_count += 1
+
+            else:
+                buffer.lost_logic_states.append(logic_states)
+                buffer.lost_actions.append(actions)
+                buffer.lost_rewards.append(rewards)
+
+            win_rates.append(win_count / (i + 1e-20))
+            # start a new game
+            game_env = create_getout_instance(args)
+
+        buffer.win_rates = win_rates
+        buffer.save_data()
+
+    return
+
+
+def collect_data_assault(agent, args):
+    sample_num = 500
+    args.filename = args.m + '_' + args.teacher_agent + '_sample_num_' + str(sample_num) + '.json'
+    buffer = RolloutBuffer(args.filename)
+    if os.path.exists(buffer.filename):
+        return
+
+    game_i = 0
+    collected_states = 0
+    win_count = 0
+    win_rates = []
+    env = OCAtari(args.m, mode="vision", hud=True, render_mode="rgb_array")
+    obs, info = env.reset()
+    current_lives = args.max_lives
+    terminated = False
+    truncated = False
+    for i in tqdm(range(sample_num), desc=f"Collect Data from game: Assault"):
+        # step game
+        dead = False
+        scored = False
+        logic_states = []
+        actions = []
+        rewards = []
+        env_objs = []
+        frame_i = 0
+        # states for one live
+        while not dead and not scored and not terminated and not truncated:
+            action, _ = agent(env.dqn_obs)
+            logic_state = extract_logic_state_assault(env.objects, args)
+            obs_prev = obs
+            obs, frame_reward, terminated, truncated, info = env.step(action)
+
+            reward = args.zero_reward
+            env_objs.append(env.objects)
+
+            # give negative reward
+            if frame_reward < 0:
+                reward = args.reward_lost_one_live
+                dead = True
+
+            # scoring state
+            elif frame_reward > 0:
+                reward = args.reward_score_one_enemy
+                if logic_state[:, 0].sum() == 0:
+                    reward = args.reward_lost_one_live
+                scored = True
+                win_count += 1
+
+            actions.append(action)
+            logic_states.append(logic_state.tolist())
+            rewards.append(reward)
+
+            Image.fromarray(game_utils.zoom_image(obs, obs.shape[0] * 3, obs.shape[1] * 3)).save(
+                args.game_buffer_path / f'{i}_{frame_i}.png_{dead}_{scored}.png', "PNG")
+            frame_i += 1
+
+        win_rates.append(win_count / (i + 1 + 1e-20))
+        if dead:
+            buffer.lost_logic_states.append(logic_states)
+            buffer.lost_actions.append(actions)
+            buffer.lost_rewards.append(rewards)
+        elif scored:
+            buffer.logic_states.append(logic_states)
+            buffer.actions.append(actions)
+            buffer.rewards.append(rewards)
+
+        if terminated or truncated:
+            terminated = False
+            truncated = False
+            env.reset()
+
+    buffer.win_rates = win_rates
+    buffer.save_data()
+
+
+def collect_data_asterix(agent, args):
+    max_games = 50
+    args.filename = args.m + '_' + args.teacher_agent + str(max_games) + '.json'
+    buffer = RolloutBuffer(args.filename)
+    if os.path.exists(buffer.filename):
+        return
+    win_rates = []
+    win_count = 0
+    env = OCAtari(args.m, mode="vision", hud=True, render_mode="rgb_array")
+
+    for i in tqdm(range(max_games)):
+        observation, info = env.reset()
+        env_args = EnvArgs(args=args, game_num=300, window_size=observation.shape[:2], fps=60)
+        frame_i = 0
+        terminated = False
+        truncated = False
+        logic_states = []
+        actions = []
+        rewards = []
+        while env_args.current_lives > 0:
+            logic_state = extract_logic_state_atari(env.objects, args.game_info)
+            action, _ = agent(env.dqn_obs)
+            obs, reward, terminated, truncated, info = env.step(action)
+            reward = game_utils.asterix_patches(env_args, reward, lives=info["lives"])
+
+            logic_states.append(logic_state.tolist())
+            actions.append(action)
+            rewards.append(reward)
+            frame_i = game_utils.update_game_args(frame_i, env_args, reward)
+
+        buffer.logic_states.append(logic_states)
+        buffer.actions.append(actions)
+        buffer.rewards.append(rewards)
+        win_count += 1
+        win_rates.append(win_count / (i + 1e-20))
+
+    buffer.win_rates = win_rates
+    buffer.save_data()
+
+
+def collect_data_game(agent, args):
+    if args.m == 'getout':
+        collect_data_getout(agent, args)
+    elif args.m == 'getoutplus':
+        collect_data_getout(agent, args)
+    elif args.m == 'Assault':
+        collect_data_assault(agent, args)
+    elif args.m == "Asterix":
+        collect_data_asterix(agent, args)
+    else:
+        raise ValueError
---
+++ pi/game_render.py
@@ -0,0 +1,238 @@
+# Created by shaji at 12/02/2024
+import time
+
+import torch
+from PIL import ImageDraw, Image
+
+from ocatari import OCAtari
+
+from pi.utils import game_utils, draw_utils
+from pi.utils.EnvArgs import EnvArgs
+from pi.utils.oc_utils import extract_logic_state_asterix, extract_logic_state_atari
+from src.utils_game import render_getout
+
+
+def render_asterix(agent, args):
+    env = OCAtari(args.m, mode="revised", hud=True, render_mode='rgb_array')
+    obs, info = env.reset()
+    env_args = EnvArgs(args=args, game_num=300, window_size=obs.shape[:2], fps=60)
+    video_out = game_utils.get_game_viewer(env_args)
+    explaining = None
+    db_plots = []
+    while env_args.game_i < env_args.game_num:
+        obs, info = env.reset()
+        env_args = EnvArgs(args=args, game_num=300, window_size=obs.shape[:2], fps=60)
+        frame_i = 0
+        decision_history = []
+        reward = 0
+
+        while env_args.current_lives > 0:
+            current_frame_time = time.time()
+            # limit frame rate
+            if env_args.last_frame_time + env_args.target_frame_duration > current_frame_time:
+                sl = (env_args.last_frame_time + env_args.target_frame_duration) - current_frame_time
+                time.sleep(sl)
+                continue
+            env_args.last_frame_time = current_frame_time  # save frame start time for next iteration
+            if agent.agent_type == "smp":
+                action, explaining = agent.act(env.objects)
+            elif agent.agent_type == "pretrained":
+                action, _ = agent(env.dqn_obs)
+            else:
+                raise ValueError
+            obs, reward, terminated, truncated, info = env.step(action)
+
+            ram = env._env.unwrapped.ale.getRAM()
+            reward = game_utils.asterix_patches(env_args, reward, info["lives"])
+            logic_state = extract_logic_state_atari(env.objects, args.game_info)
+            agent_num = int(logic_state[:, 0].sum())
+            enemy_num = int(logic_state[:, 1].sum())
+            cauldron_num = int(logic_state[:, 2].sum())
+            helmet_num = int(logic_state[:, 3].sum())
+            print(
+                f"g: {env_args.game_i}, f: {frame_i}, rw: {reward}, act: {action}, lives:{info['lives']}, "
+                f"agent: {agent_num}, enemy: {enemy_num}, cauldron: {cauldron_num}, helmet: {helmet_num}")
+            if explaining is not None:
+                explaining["reward"].append(reward)
+                decision_history.append(explaining)
+            # render the game
+            wr_plot = game_utils.plot_wr(env_args)
+            mt_plot = game_utils.plot_mt_asterix(env_args, agent)
+            video_out = game_utils.plot_game_frame(env_args, video_out, obs, wr_plot, mt_plot, db_plots)
+
+            frame_i = game_utils.update_game_args(frame_i, env_args, reward)
+
+        # revise the game rules
+        if len(decision_history) > 2 and reward < 0:
+            lost_game_data = agent.revise_loss(decision_history)
+            agent.update_lost_buffer(lost_game_data)
+            def_behaviors, db_plots = agent.reasoning_def_behaviors(use_ckp=False)
+            agent.update_behaviors(None, def_behaviors, None, args)
+            print("- revise loss finished.")
+    env.close()
+    draw_utils.release_video(video_out)
+
+
+def render_kangaroo(agent, args):
+    env = OCAtari(args.m, mode="vision", hud=True, render_mode='rgb_array')
+    obs, info = env.reset()
+    env_args = EnvArgs(args=args, game_num=300, window_size=obs.shape[:2], fps=60)
+    video_out = game_utils.get_game_viewer(env_args)
+    explaining = None
+    db_plots = []
+    while env_args.game_i < env_args.game_num:
+        frame_i = 0
+        terminated = False
+        truncated = False
+        decision_history = []
+        reward = 0
+        current_lives = args.max_lives
+        while not terminated or truncated:
+            current_frame_time = time.time()
+            # limit frame rate
+            if env_args.last_frame_time + env_args.target_frame_duration > current_frame_time:
+                sl = (env_args.last_frame_time + env_args.target_frame_duration) - current_frame_time
+                time.sleep(sl)
+                continue
+            env_args.last_frame_time = current_frame_time  # save frame start time for next iteration
+            if agent.agent_type == "smp":
+                action, explaining = agent.act(env.objects)
+            elif agent.agent_type == "pretrained":
+                action, _ = agent(env.dqn_obs)
+            else:
+                raise ValueError
+            obs, reward, terminated, truncated, info = env.step(action)
+
+            ram = env._env.unwrapped.ale.getRAM()
+            reward = game_utils.kangaroo_patches(env_args, reward, info["lives"])
+            logic_state = extract_logic_state_atari(env.objects, args.game_info)
+            if explaining is not None:
+                explaining["reward"].append(reward)
+                decision_history.append(explaining)
+            # render the game
+            wr_plot = game_utils.plot_wr(env_args)
+            mt_plot = game_utils.plot_mt_asterix(env_args, agent)
+            video_out = game_utils.plot_game_frame(env_args, video_out, obs, wr_plot, mt_plot, db_plots)
+
+            frame_i = game_utils.update_game_args(frame_i, env_args, reward)
+
+        # finish one game
+        if terminated or truncated:
+            env.reset()
+
+        # revise the game rules
+        if len(decision_history) > 2 and reward < 0:
+            lost_game_data = agent.revise_loss(decision_history)
+            agent.update_lost_buffer(lost_game_data)
+            def_behaviors, db_plots = agent.reasoning_def_behaviors(use_ckp=False)
+            agent.update_behaviors(None, def_behaviors, None, args)
+            print("- revise loss finished.")
+    env.close()
+    draw_utils.release_video(video_out)
+
+
+def render_game(agent, args):
+    if args.m == 'getout' or args.m == "getoutplus":
+        render_getout(agent, args)
+    elif args.m == 'Assault':
+        render_assault(agent, args)
+    elif args.m == 'Asterix':
+        render_asterix(agent, args)
+    elif args.m == "Kangaroo":
+        render_kangaroo(agent, args)
+    else:
+        raise ValueError("Game not exist.")
+
+
+def render_assault(agent, args):
+    env = OCAtari(args.m, mode="vision", hud=True, render_mode='rgb_array')
+    game_num = 300
+    game_i = 0
+    win_counter = 0
+    win_rate = torch.zeros(2, game_num)
+    win_rate[1, :] = agent.buffer_win_rates[:game_num]
+    observation, info = env.reset()
+    zoom_in = args.zoom_in
+    viewer = game_utils.setup_image_viewer(env.game_name, zoom_in * observation.shape[0],
+                                           zoom_in * observation.shape[1])
+
+    # frame rate limiting
+    fps = 60
+    target_frame_duration = 1 / fps
+    last_frame_time = 0
+
+    explaining = {}
+    while game_i < game_num:
+        frame_i = 0
+        terminated = False
+        truncated = False
+        decision_history = []
+        current_lives = args.max_lives
+
+        while not terminated or truncated:
+            current_frame_time = time.time()
+            # limit frame rate
+            if last_frame_time + target_frame_duration > current_frame_time:
+                sl = (last_frame_time + target_frame_duration) - current_frame_time
+                time.sleep(sl)
+                continue
+            last_frame_time = current_frame_time  # save frame start time for next iteration
+
+            if args.agent_type == "smp":
+                action, explaining = agent.act(env.objects)
+            elif args.agent_type == "ppo":
+                action, _ = agent(env.dqn_obs)
+            else:
+                raise ValueError
+
+            obs, reward, terminated, truncated, info = env.step(action)
+            if info['lives'] < current_lives:
+                reward = args.reward_lost_one_live
+                current_lives = info['lives']
+
+            # logging
+            # for beh_i in explaining['behavior_index']:
+            #     print(f"f: {game_i}, rw: {reward}, act: {action}, behavior: {agent.behaviors[beh_i].clause}")
+
+            # visualization
+            if frame_i % 5 == 0:
+                if args.render:
+                    ImageDraw.Draw(Image.fromarray(obs)).text((40, 60), f"ep: {game_i}, win: {win_counter}",
+                                                              (120, 20, 20))
+                    zoom_obs = game_utils.zoom_image(obs, zoom_in * observation.shape[0],
+                                                     zoom_in * observation.shape[1])
+                    viewer.show(zoom_obs[:, :, :3])
+
+            if reward < 0:
+                game_i += 1
+                frame_i = 0
+                win_rate[0, game_i] = win_counter / (game_i + 1e-20)
+
+            elif reward > 0:
+                win_counter += 1
+                game_i += 1
+                frame_i = 0
+                win_rate[0, game_i] = win_counter / (game_i + 1e-20)
+
+            ram = env._env.unwrapped.ale.getRAM()
+            if explaining is not None:
+                explaining["reward"].append(reward)
+                decision_history.append(explaining)
+
+            # finish one game
+            if terminated or truncated:
+                observation, info = env.reset()
+
+            # the game total frame number has to greater than 2
+            if len(decision_history) > 2 and reward < 0:
+                lost_game_data = agent.revise_loss(decision_history)
+                agent.update_lost_buffer(lost_game_data)
+                def_behaviors = agent.reasoning_def_behaviors(use_ckp=False)
+                agent.update_behaviors(None, def_behaviors, None, args)
+                decision_history = []
+                print("- revise loss finished.")
+            frame_i += 1
+            draw_utils.plot_line_chart(win_rate[:, :game_i], args.output_folder, ['smp', 'ppo'], title='win_rate',
+                                       cla_leg=True)
+        # modify and display render
+    env.close()
