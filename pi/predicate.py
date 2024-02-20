1. weighting predicates by using the confidence of each sort of training data. 2. Asterix gets 1500 scores in average (max 4200) 2. fix bugs

pi/predicate.py
@@ -207,11 +207,12 @@ class Dist_Closest():
     """ generate one micro-program
     """
 
-    def __init__(self, args, dist_range, dir_avg, name, plot_path):
+    def __init__(self, args, dist_range, dir_range, dir_conf, name, plot_path):
         super().__init__()
         self.args = args
         self.dist_range = dist_range
-        self.dir_avg = dir_avg
+        self.dir_range = dir_range
+        self.dir_conf = dir_conf
         self.model = None
         self.num_epochs = args.train_nn_epochs
         self.name = f"{name}"
@@ -301,10 +302,17 @@ class Dist_Closest():
 
         mask_x = (dist[:, 0] <= x_max) * (dist[:, 0] >= x_min)
         mask_y = (dist[:, 1] <= y_max) * (dist[:, 1] >= y_min)
-        mask_dir = dir.squeeze() == self.dir_avg
+        mask_dir = torch.zeros(len(mask_x), dtype=torch.bool).to(t2.device)
+        conf_dir = torch.zeros(len(mask_x)).to(t2.device)
+        for d_i, d in enumerate(dir):
+            if d in self.dir_range:
+                mask_dir[d_i] = True
+                conf_dir[d_i] = self.dir_conf[self.dir_range == d]
+
         mask = mask_x * mask_y * mask_dir
+        conf_dir[~mask] = 0
         # Use the trained model to predict the new value
-        new_value_prediction = mask.sum() / (mask.sum() + 1e-20)
+        new_value_prediction = conf_dir.max()
         # satisfactions = new_value_prediction.argmax(dim=1) == self.y_0
 
         return new_value_prediction

pi/utils/beh_utils.py
@@ -53,7 +53,8 @@ def create_positive_behaviors(args, pos_beh_data):
 def create_negative_behavior(args, beh_i, beh):
     # create defense behaviors
     dist_range_pos = torch.tensor(beh["dist_range"], dtype=torch.float32)
-    dir_avg = torch.tensor(beh["dir_degree"], dtype=torch.float32)
+    dir_range_pos = torch.tensor(beh["dir_range"], dtype=torch.float32)
+    dir_conf_pos = torch.tensor(beh["dir_conf"], dtype=torch.float32)
 
     dist_pos = torch.tensor(beh["dists_pos"], dtype=torch.float32)
     dir_pos = torch.tensor(beh["dir_pos"], dtype=torch.float32)
@@ -69,13 +70,15 @@ def create_negative_behavior(args, beh_i, beh):
     mask = beh["masks"]
 
     # create predicate
-    dir_name = math_utils.pol2dir_name(dir_avg)
+    dir_names = []
+    for dir in dir_range_pos:
+        dir_names.append(math_utils.pol2dir_name(dir))
 
-    pred_name = (f"dir_{dir_name}_xi{dist_range_pos[0][0]:.2f}_xa{dist_range_pos[0][1]:.2f}_"
-                 f"yi_{dist_range_pos[1][0]:.2f}_ya{dist_range_pos[1][0]:.2f}")
+    pred_name = (f"dir_{dir_names}_xi{dist_range_pos[0][0]:.2f}_xa{dist_range_pos[0][1]:.2f}_"
+                 f"yi_{dist_range_pos[1][0]:.2f}_ya{dist_range_pos[1][1]:.2f}")
 
-    dist_pred = predicate.Dist_Closest(args, dist_range=dist_range_pos, dir_avg=dir_avg, name=pred_name,
-                                       plot_path=args.check_point_path / "defensive")
+    dist_pred = predicate.Dist_Closest(args, dist_range=dist_range_pos, dir_range=dir_range_pos, dir_conf=dir_conf_pos,
+                                       name=pred_name, plot_path=args.check_point_path / "defensive")
     pred = [dist_pred]
 
     beh_fact = VarianceFact(mask, obj_combs, prop_combs, pred)
@@ -107,7 +110,7 @@ def update_negative_behaviors(args, behaviors, def_beh_data):
         #         break
         if not behavior_exist:
             behavior = create_negative_behavior(args, data_i, beh_data)
-            print(f"- new behavior {behavior.clause}")
+            # print(f"- new behavior {behavior.clause}")
             defense_behaviors.append(behavior)
     return defense_behaviors
 
@@ -115,7 +118,7 @@ def update_negative_behaviors(args, behaviors, def_beh_data):
 def create_pf_behavior(args, beh_i, beh):
     # create attack behaviors
     dist_range_pos = torch.tensor(beh["dist_range"], dtype=torch.float32)
-    dir_avg = torch.tensor(beh["dir_degree"], dtype=torch.float32)
+    dir_range_pos = torch.tensor(beh["dir_range"], dtype=torch.float32)
 
     expected_reward = beh["rewards"]
     obj_combs = beh["obj_combs"]
@@ -124,13 +127,15 @@ def create_pf_behavior(args, beh_i, beh):
     mask = beh["masks"]
 
     # create predicate
-    dir_name = math_utils.pol2dir_name(dir_avg)
-    pred_name = (f"dir_{dir_name}_xi{dist_range_pos[0][0]:.2f}_xa{dist_range_pos[0][1]:.2f}_"
-                 f"yi_{dist_range_pos[1][0]:.2f}_ya{dist_range_pos[1][0]:.2f}")
+    dir_names = []
+    for dir in dir_range_pos:
+        dir_names.append(math_utils.pol2dir_name(dir))
+    pred_name = (f"dir_{dir_names}_xi{dist_range_pos[0][0]:.2f}_xa{dist_range_pos[0][1]:.2f}_"
+                 f"yi_{dist_range_pos[1][0]:.2f}_ya{dist_range_pos[1][1]:.2f}")
     # dir_pos = torch.cat((dir_pos), dim=1)
     # dir_neg = torch.cat((dir_neg, pos_neg), dim=1)
 
-    dist_pred = predicate.Dist_Closest(args, dist_range=dist_range_pos, dir_avg=dir_avg, name=pred_name,
+    dist_pred = predicate.Dist_Closest(args, dist_range=dist_range_pos, dir_range=dir_range_pos, name=pred_name,
                                        plot_path=args.check_point_path / "path_finding")
     pred = [dist_pred]
 
@@ -148,7 +153,9 @@ def create_pf_behavior(args, beh_i, beh):
 def create_attack_behavior(args, beh_i, beh):
     # create attack behaviors
     dist_range_pos = torch.tensor(beh["dist_range"], dtype=torch.float32)
-    dir_avg = torch.tensor(beh["dir_degree"], dtype=torch.float32)
+    dir_range_pos = torch.tensor(beh["dir_range"], dtype=torch.float32)
+    dir_conf_pos = torch.tensor(beh["dir_conf"], dtype=torch.float32)
+
     pos_pos = torch.tensor(beh["position_pos"], dtype=torch.float32)
     pos_neg = torch.tensor(beh["position_neg"], dtype=torch.float32)
     expected_reward = beh["rewards"]
@@ -158,13 +165,15 @@ def create_attack_behavior(args, beh_i, beh):
     mask = beh["masks"]
 
     # create predicate
-    dir_name = math_utils.pol2dir_name(dir_avg)
+    dir_names = []
+    for dir in dir_range_pos:
+        dir_names.append(math_utils.pol2dir_name(dir))
 
-    pred_name = (f"dir_{dir_name}_xi{dist_range_pos[0][0]:.2f}_xa{dist_range_pos[0][1]:.2f}_"
-                 f"yi_{dist_range_pos[1][0]:.2f}_ya{dist_range_pos[1][0]:.2f}")
+    pred_name = (f"dir_{dir_names}_xi{dist_range_pos[0][0]:.2f}_xa{dist_range_pos[0][1]:.2f}_"
+                 f"yi_{dist_range_pos[1][0]:.2f}_ya{dist_range_pos[1][1]:.2f}")
 
-    dist_pred = predicate.Dist_Closest(args, dist_range=dist_range_pos, dir_avg=dir_avg, name=pred_name,
-                                       plot_path=args.check_point_path / "attack")
+    dist_pred = predicate.Dist_Closest(args, dist_range=dist_range_pos, dir_range=dir_range_pos, dir_conf=dir_conf_pos,
+                                       name=pred_name, plot_path=args.check_point_path / "attack")
     pred = [dist_pred]
 
     beh_fact = VarianceFact(mask, obj_combs, prop_combs, pred)

pi/utils/math_utils.py
@@ -12,7 +12,7 @@ def calculate_direction(points, reference_point):
 
         x, y = points[r_i].squeeze()
         delta_x = x - x_ref
-        delta_y = y - y_ref
+        delta_y = y_ref- y
 
         angle_radians = torch.atan2(delta_y, delta_x)
         angle_degrees = math.degrees(angle_radians)
@@ -58,7 +58,7 @@ def get_90_percent_range_2d(data):
 
     ranges = np.zeros((data.shape[-1], 2))
     for i in range(data.shape[-1]):
-        ranges[i] = np.percentile(data[:, i], [5, 95])
+        ranges[i] = np.percentile(data[:, i], [2, 98])
     return ranges
 
 
@@ -99,7 +99,7 @@ def dist_a_and_b_closest(data_A, data_B):
         _, closest_index = torch.abs(dist_all).min(dim=1)
         dist = torch.zeros(data_B.size(0), data_B.size(2))
         for i in range(closest_index.size(0)):
-            dist[i] = diff_abs[i, closest_index[i], :]
+            dist[i] = torch.abs(diff_abs[i, closest_index[i], :])
     else:
         dist = data_A - data_B
 

pi/utils/smp_utils.py
@@ -907,7 +907,7 @@ def stat_rewards(states, actions, rewards, zero_reward, game_info, prop_indices,
         obj_combs = torch.tensor(list(itertools.product(obj_0_indices, obj_1_indices)))
         obj_a_indices = obj_combs[:, 0].unique()
         obj_b_indices = obj_combs[:, 1].unique()
-        if len(obj_a_indices) == 1 and states_action_pos.shape[0] > 2:
+        if len(obj_a_indices) == 1 and states_action_pos.shape[0] > 8:
             action_name = action_names[action_type]
             action_dir = math_utils.action_to_deg(action_name)
 
@@ -971,25 +971,26 @@ def stat_rewards(states, actions, rewards, zero_reward, game_info, prop_indices,
         dist_range = math_utils.get_90_percent_range_2d(state_stat["dists_pos"].numpy())
         if np.abs(dist_range).max() > 0.1:
             continue
-        if state_stat["dir_pos"].max()>1:
+        if state_stat["dir_pos"].max() > 1:
             print("")
         dir_value = state_stat["dir_pos"]
         dir_quarter_value = math_utils.closest_quarter(dir_value)
-
-
-        dir_degree = math_utils.get_frequnst_value(dir_quarter_value)
+        dir_quarter_values, dir_counts = dir_quarter_value.unique(return_counts=True)
+        dir_conf = dir_counts / dir_counts.sum()
 
         action_id = state_stat["action_type"].tolist()
         action_value = math_utils.action_to_deg(action_names[action_id])
-        if not action_value == dir_degree and action_value != 100:
-            print(f"aciton value {action_value}, data direction: {dir_degree} variance: {state_stat['variances'].tolist()}")
+        if action_value not in dir_quarter_values.tolist() and action_value != 100:
+            print(f"aciton value {action_value}, data direction: {dir_quarter_values} "
+                  f"variance: {state_stat['variances'].tolist()}")
             print("")
 
         # dir_degree = math_utils.range_to_direction(dir_range.squeeze())
         # symbolize the data further with specific direction and distance
         behs.append({
             "dist_range": dist_range.tolist(),
-            "dir_degree": dir_degree.tolist(),
+            "dir_range": dir_quarter_values.tolist(),
+            "dir_conf": dir_conf.tolist(),
             "dists_pos": state_stat["dists_pos"].tolist(),
             "dir_pos": state_stat["dir_pos"].tolist(),
             "position_pos": state_stat["position_pos"].tolist(),