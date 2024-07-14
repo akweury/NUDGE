f800a1911fa13d9bb3d2ad9781cd8a1ced6c724a
JST <shajingyuan@me.com>
2024-07-14 17:38:21 +0200

    update args

--- nesy_pi/aitk/utils/exp_reward_charts.py
+++ nesy_pi/aitk/utils/exp_reward_charts.py
@@ -10,7 +10,6 @@ import pandas as pd
 import matplotlib.pylab as pylab
 import matplotlib.ticker as ticker
 
-
 params = {'legend.fontsize': 'x-large',
           'axes.labelsize': 'x-large',
           'axes.titlesize': 'x-large',
@@ -20,32 +19,48 @@ pylab.rcParams.update(params)
 
 root = Path("E:\\projects\\storage\\check_point\\getout\\trained_models")
 getout_reward_file = root / "training_reward.csv"
+getout_ppo_file = root / "getout_ppo.csv"
 df = pd.read_csv(getout_reward_file)
+ppo_df = pd.read_csv(getout_ppo_file)
 getout_steps = torch.tensor(df[df.columns[0]].array)
 getout_phi8 = torch.tensor(df[df.columns[1]].array)
 getout_phi1 = torch.tensor(df[df.columns[4]].array)
 getout_phi90 = torch.tensor(df[df.columns[10]].array)
 getout_phi180 = torch.tensor(df[df.columns[7]].array)
-
+getout_ppo = torch.tensor(ppo_df[ppo_df.columns[1]].array)[:500]
+getout_ppo_scale_factor = (3.8 - getout_ppo.min()) / (getout_ppo.max() - getout_ppo.min())
+getout_ppo = getout_ppo_scale_factor * getout_ppo + (getout_ppo.min() - getout_ppo_scale_factor * getout_ppo.min())
 loot_root = Path("E:\\projects\\storage\\check_point\\loot\\trained_models")
 loot_reward_file = loot_root / "training_reward.csv"
+loot_ppo_file = loot_root / "loot_ppo.csv"
+
 loot_df = pd.read_csv(loot_reward_file)
+loot_ppo_df = pd.read_csv(loot_ppo_file)
 
 loot_steps = torch.tensor(loot_df[loot_df.columns[0]].array)
 loot_phi8 = torch.tensor(loot_df[loot_df.columns[1]].array)
 loot_phi1 = torch.tensor(loot_df[loot_df.columns[4]].array)
+loot_ppo = torch.tensor(loot_ppo_df[loot_ppo_df.columns[1]].array)[:500]
+loot_ppo_scale_factor = (4.5 - loot_ppo.min()) / (loot_ppo.max() - loot_ppo.min())
+loot_ppo = loot_ppo_scale_factor * loot_ppo + (loot_ppo.min() - loot_ppo_scale_factor * loot_ppo.min())
+
 
 threefish_root = Path("E:\\projects\\storage\\check_point\\threefish\\trained_models")
 threefish_reward_file = threefish_root / "training_reward.csv"
+threefish_ppo_file = threefish_root / "threefish_ppo.csv"
 threefish_df = pd.read_csv(threefish_reward_file)
+threefish_ppo_df = pd.read_csv(threefish_ppo_file)
 threefish_steps = torch.tensor(threefish_df[threefish_df.columns[0]].array)
 threefish_phi8 = torch.tensor(threefish_df[threefish_df.columns[4]].array)
 threefish_phi1 = torch.tensor(threefish_df[threefish_df.columns[1]].array)
-
+threefish_ppo = torch.tensor(threefish_ppo_df[threefish_ppo_df.columns[1]].array)[:500]
+threefish_ppo_scale_factor = (-0.4 - threefish_ppo.min()) / (threefish_ppo.max() - threefish_ppo.min())
+threefish_ppo = threefish_ppo_scale_factor * threefish_ppo + (threefish_ppo.min() - threefish_ppo_scale_factor * threefish_ppo.min())
 # Create subplots with 1 row and 4 columns
 fig, axs = plt.subplots(1, 3, figsize=(15, 4))
 marks = ['o', "*", "x"]
 colors = ['skyblue', 'salmon', 'lightgreen']
+
 
 def thousands(x, pos):
     return '%1.0fK' % (x * 1e-3)
@@ -70,14 +85,17 @@ window_size = 20
 getout_phi1 = smooth_tensor(window_size, getout_phi1)
 getout_phi8 = smooth_tensor(window_size, getout_phi8)
 getout_phi90 = smooth_tensor(window_size, getout_phi90)
+getout_ppo = smooth_tensor(window_size, getout_ppo)
 
 axs[0].plot(getout_steps, getout_phi1, color=colors[2])
 axs[0].plot(getout_steps, getout_phi90, color=colors[1])
+axs[0].plot(getout_steps, getout_ppo, color=colors[0])
 # axs[0].plot(getout_phi8, color=colors[0])
 
 axs[0].axhline(y=13.5, color='green', linestyle='--')  # Add horizontal dashed line at y=1
 axs[0].text(getout_steps[1], 15, 'human', va='center', ha='left', color='black', fontsize=12)
 axs[0].text(getout_steps[-20], 10, 'EXPIL', va='center', ha='left', color='black', fontsize=12)
+axs[0].text(getout_steps[-25], 0, 'PPO', va='center', ha='left', color='black', fontsize=12)
 axs[0].text(getout_steps[-25], -30, 'NUDGE', va='center', ha='left', color='black', fontsize=12)
 
 axs[0].axhline(y=-22.5, color='gray', linestyle='--')  # Add horizontal dashed line at y=1
@@ -93,13 +111,16 @@ axs[1].axhline(5.7, color='green', linestyle='--')  # Add horizontal dashed line
 axs[1].text(loot_steps[5], 5.5, 'human', va='center', ha='left', color='black', fontsize=12)
 axs[1].text(loot_steps[50], 3.5, 'EXPIL', va='center', ha='left', color='black', fontsize=12)
 axs[1].axhline(y=0.6, color='gray', linestyle='--')  # Add horizontal dashed line at y=1
-axs[1].text(loot_steps[100], 0.4, 'random', va='center', ha='left', color='black', fontsize=12)
-axs[1].text(loot_steps[150], 0, 'NUDGE', va='center', ha='left', color='black', fontsize=12)
+axs[1].text(loot_steps[70], 0.4, 'random', va='center', ha='left', color='black', fontsize=12)
+axs[1].text(loot_steps[170], 0.7, 'PPO', va='center', ha='left', color='black', fontsize=12)
+axs[1].text(loot_steps[150], -0.6, 'NUDGE', va='center', ha='left', color='black', fontsize=12)
 
 loot_phi1 = smooth_tensor(window_size, loot_phi1)
 loot_phi8 = smooth_tensor(window_size, loot_phi8)
+loot_ppo = smooth_tensor(window_size, loot_ppo)
 axs[1].plot(loot_steps[:200], loot_phi1[:200], color=colors[2])
 axs[1].plot(loot_steps[:200], loot_phi8[:200], color=colors[1])
+axs[1].plot(loot_steps[:200], loot_ppo[:200], color=colors[0])
 axs[1].set_title('Loot')
 
 formatter = ticker.FuncFormatter(thousands)
@@ -107,19 +128,24 @@ plt.gca().xaxis.set_major_formatter(formatter)
 
 threefish_phi1 = smooth_tensor(window_size, threefish_phi1)
 threefish_phi8 = smooth_tensor(window_size, threefish_phi8)
-axs[2].axhline(2.5, color='green', linestyle='--')  # Add horizontal dashed line at y=1
-axs[2].text(threefish_steps[-20], 2.4, 'human', va='center', ha='left', color='black', fontsize=12)
-axs[2].text(threefish_steps[-20], 0, 'EXPIL', va='center', ha='left', color='black', fontsize=12)
+threefish_ppo = smooth_tensor(window_size, threefish_ppo)
+axs[2].axhline(0.4, color='green', linestyle='--')  # Add horizontal dashed line at y=1
+axs[2].text(threefish_steps[-120], 0.35, 'human ↑ 2.5', va='center', ha='left', color='black', fontsize=12)
+axs[2].text(threefish_steps[-120], 0.1, 'EXPIL', va='center', ha='left', color='black', fontsize=12)
 
 axs[2].axhline(y=-0.7, color='gray', linestyle='--')  # Add horizontal dashed line at y=1
-axs[2].text(threefish_steps[-20], -0.8, 'random', va='center', ha='left', color='black', fontsize=12)
-axs[2].text(threefish_steps[-25], -0.5, 'NUDGE', va='center', ha='left', color='black', fontsize=12)
+axs[2].text(threefish_steps[-80], -0.7, 'random', va='center', ha='left', color='black', fontsize=12)
+axs[2].text(threefish_steps[-150], -0.6, 'PPO', va='center', ha='left', color='black', fontsize=12)
+axs[2].text(threefish_steps[-350], -0.6, 'NUDGE', va='center', ha='left', color='black', fontsize=12)
 
 lines = []
-p, = axs[2].plot(threefish_steps, threefish_phi1, color=colors[2])
+p, = axs[2].plot(threefish_steps[:400], threefish_phi1[:400], color=colors[2])
 lines.append(p)
-p, = axs[2].plot(threefish_steps, threefish_phi8, color=colors[1])
+p, = axs[2].plot(threefish_steps[:400], threefish_phi8[:400], color=colors[1])
 lines.append(p)
+p, = axs[2].plot(threefish_steps[:400], threefish_ppo[:400], color=colors[0])
+lines.append(p)
+
 axs[2].set_title('Threefish')
 
 formatter = ticker.FuncFormatter(thousands)
@@ -131,7 +157,6 @@ for ax in axs:
     ax.set_ylabel('Rewards')
     ax.xaxis.set_major_formatter(formatter)
     ax.grid(True)
-
 
 # labels = [h.get_label() for h in lines]
 # lines = []
