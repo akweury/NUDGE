639724bf42e67fd9cc29d6e0bf60b0ae24aa1c56
jing <shajingyuan@me.com>
2024-08-13 14:57:34 +0200

    update args

--- nesy_pi/aitk/utils/exp_reward_charts_deep_compare.py
+++ nesy_pi/aitk/utils/exp_reward_charts_deep_compare.py
@@ -17,44 +17,6 @@ params = {'legend.fontsize': 'x-large',
           'xtick.labelsize': 'x-large',
           'ytick.labelsize': 'x-large'}
 pylab.rcParams.update(params)
-
-loot_reward_file = Path(config.path_nesy / "wandb" / "wandb_reward_loot.csv")
-threefish_reward_file = Path(config.path_nesy / "wandb" / "wandb_reward_loot.csv")
-
-#
-# df = pd.read_csv(getout_reward_file)
-# ppo_df = pd.read_csv(getout_ppo_file)
-# getout_steps = torch.tensor(df[df.columns[0]].array)
-# getout_phi8 = torch.tensor(df[df.columns[1]].array)
-# getout_phi1 = torch.tensor(df[df.columns[4]].array)
-# getout_phi90 = torch.tensor(df[df.columns[10]].array)
-# getout_phi180 = torch.tensor(df[df.columns[7]].array)
-# getout_ppo = torch.tensor(ppo_df[ppo_df.columns[1]].array)[:500]
-# getout_ppo_scale_factor = (3.8 - getout_ppo.min()) / (getout_ppo.max() - getout_ppo.min())
-# getout_ppo = getout_ppo_scale_factor * getout_ppo + (getout_ppo.min() - getout_ppo_scale_factor * getout_ppo.min())
-
-loot_df = pd.read_csv(loot_reward_file)
-loot_steps = torch.tensor(loot_df[loot_df.columns[0]].array)
-loot_rho8 = torch.tensor(loot_df[loot_df.columns[1]].array)
-loot_phi8 = torch.tensor(loot_df[loot_df.columns[4]].array)
-loot_phirho8 = torch.tensor(loot_df[loot_df.columns[7]].array)
-loot_phi10 = torch.tensor(loot_df[loot_df.columns[10]].array)
-loot_phi1 = torch.tensor(loot_df[loot_df.columns[16]].array)
-
-#
-# threefish_root = Path("E:\\projects\\storage\\check_point\\threefish\\trained_models")
-# threefish_reward_file = threefish_root / "training_reward.csv"
-# threefish_ppo_file = threefish_root / "threefish_ppo.csv"
-# threefish_df = pd.read_csv(threefish_reward_file)
-# threefish_ppo_df = pd.read_csv(threefish_ppo_file)
-# threefish_steps = torch.tensor(threefish_df[threefish_df.columns[0]].array)
-# threefish_phi8 = torch.tensor(threefish_df[threefish_df.columns[4]].array)
-# threefish_phi1 = torch.tensor(threefish_df[threefish_df.columns[1]].array)
-# threefish_ppo = torch.tensor(threefish_ppo_df[threefish_ppo_df.columns[1]].array)[:500]
-# threefish_ppo_scale_factor = (-0.4 - threefish_ppo.min()) / (threefish_ppo.max() - threefish_ppo.min())
-# threefish_ppo = threefish_ppo_scale_factor * threefish_ppo + (
-#             threefish_ppo.min() - threefish_ppo_scale_factor * threefish_ppo.min())
-# Create subplots with 1 row and 4 columns
 fig, axs = plt.subplots(1, 2, figsize=(15, 8))
 marks = ['o', "*", "x"]
 colors = ['skyblue', 'salmon', 'lightgreen', "orchid", "gold"]
@@ -88,6 +50,54 @@ def conf_interval(data, color):
 
 
 window_size = 20
+
+loot_reward_file = Path(config.path_nesy / "wandb" / "wandb_reward_loot.csv")
+threefish_reward_file = Path(config.path_nesy / "wandb" / "wandb_reward_threefish.csv")
+
+#
+# df = pd.read_csv(getout_reward_file)
+# ppo_df = pd.read_csv(getout_ppo_file)
+# getout_steps = torch.tensor(df[df.columns[0]].array)
+# getout_phi8 = torch.tensor(df[df.columns[1]].array)
+# getout_phi1 = torch.tensor(df[df.columns[4]].array)
+# getout_phi90 = torch.tensor(df[df.columns[10]].array)
+# getout_phi180 = torch.tensor(df[df.columns[7]].array)
+# getout_ppo = torch.tensor(ppo_df[ppo_df.columns[1]].array)[:500]
+# getout_ppo_scale_factor = (3.8 - getout_ppo.min()) / (getout_ppo.max() - getout_ppo.min())
+# getout_ppo = getout_ppo_scale_factor * getout_ppo + (getout_ppo.min() - getout_ppo_scale_factor * getout_ppo.min())
+
+loot_df = pd.read_csv(loot_reward_file)
+loot_steps = torch.tensor(loot_df[loot_df.columns[0]].array)
+loot_rho8 = torch.tensor(loot_df[loot_df.columns[1]].array)
+loot_phi8 = torch.tensor(loot_df[loot_df.columns[4]].array)
+loot_phirho8 = torch.tensor(loot_df[loot_df.columns[7]].array)
+loot_phi10 = torch.tensor(loot_df[loot_df.columns[10]].array)
+loot_phi1 = torch.tensor(loot_df[loot_df.columns[16]].array)
+
+threefish_df = pd.read_csv(threefish_reward_file)
+df = threefish_df
+threefish_steps = torch.tensor(df[df.columns[0]].array)
+threefish_rho8 = torch.tensor(df[df.columns[1]].array)
+threefish_phi8 = torch.tensor(df[df.columns[4]].array)
+threefish_phirho8 = torch.tensor(df[df.columns[7]].array)
+threefish_phi10 = torch.tensor(df[df.columns[10]].array)
+threefish_phi1 = torch.tensor(df[df.columns[16]].array)
+
+#
+# threefish_root = Path("E:\\projects\\storage\\check_point\\threefish\\trained_models")
+# threefish_reward_file = threefish_root / "training_reward.csv"
+# threefish_ppo_file = threefish_root / "threefish_ppo.csv"
+# threefish_df = pd.read_csv(threefish_reward_file)
+# threefish_ppo_df = pd.read_csv(threefish_ppo_file)
+# threefish_steps = torch.tensor(threefish_df[threefish_df.columns[0]].array)
+# threefish_phi8 = torch.tensor(threefish_df[threefish_df.columns[4]].array)
+# threefish_phi1 = torch.tensor(threefish_df[threefish_df.columns[1]].array)
+# threefish_ppo = torch.tensor(threefish_ppo_df[threefish_ppo_df.columns[1]].array)[:500]
+# threefish_ppo_scale_factor = (-0.4 - threefish_ppo.min()) / (threefish_ppo.max() - threefish_ppo.min())
+# threefish_ppo = threefish_ppo_scale_factor * threefish_ppo + (
+#             threefish_ppo.min() - threefish_ppo_scale_factor * threefish_ppo.min())
+# Create subplots with 1 row and 4 columns
+
 # getout_phi1 = smooth_tensor(window_size, getout_phi1)
 # getout_phi8 = smooth_tensor(window_size, getout_phi8)
 # getout_phi90 = smooth_tensor(window_size, getout_phi90)
@@ -142,12 +152,11 @@ loot_rho8 = smooth_tensor(window_size, loot_rho8)
 loot_phirho8 = smooth_tensor(window_size, loot_phirho8)
 
 # loot_ppo = smooth_tensor(window_size, loot_ppo)
+
+axs[axis].plot(loot_steps[:600], loot_phi10[:600], color=colors[0])
+axs[axis].plot(loot_steps[:600], loot_phi8[:600], color=colors[1])
 axs[axis].plot(loot_steps[:200], loot_phi1[:200], color=colors[2])
 axs[axis].plot(loot_steps[:600], loot_rho8[:600], color=colors[3])
-
-axs[axis].plot(loot_steps[:600], loot_phi8[:600], color=colors[1])
-axs[axis].plot(loot_steps[:600], loot_phi10[:600], color=colors[0])
-
 axs[axis].plot(loot_steps[:600], loot_phirho8[:600], color=colors[4])
 
 lb_loot_phi1, ub_loot_phi1 = conf_interval(loot_phi1[:200], colors[2])
@@ -169,6 +178,54 @@ axs[axis].set_title('Loot')
 formatter = ticker.FuncFormatter(thousands)
 plt.gca().xaxis.set_major_formatter(formatter)
 
+axis = 1
+df = pd.read_csv(threefish_reward_file)
+steps = torch.tensor(df[df.columns[0]].array)
+rho8 = torch.tensor(df[df.columns[1]].array)
+phirho8 = torch.tensor(df[df.columns[4]].array)
+phi10 = torch.tensor(df[df.columns[10]].array)
+phi1 = torch.tensor(df[df.columns[13]].array)
+phi8 = torch.tensor(df[df.columns[16]].array)
+
+# axs[axis].axhline(5.7, color='green', linestyle='--')  # Add horizontal dashed line at y=1
+# axs[axis].text(steps[5], 5.5, 'human', va='center', ha='left', color='black', fontsize=12)
+# axs[axis].text(steps[50], 3.5, 'EXPIL', va='center', ha='left', color='black', fontsize=12)
+# axs[axis].axhline(y=0.6, color='gray', linestyle='--')  # Add horizontal dashed line at y=1
+# axs[axis].text(steps[70], 0.4, 'random', va='center', ha='left', color='black', fontsize=12)
+# axs[axis].text(steps[170], 0.7, 'PPO', va='center', ha='left', color='black', fontsize=12)
+# axs[axis].text(steps[150], -0.6, 'NUDGE', va='center', ha='left', color='black', fontsize=12)
+
+phi1 = smooth_tensor(window_size, phi1)
+phi8 = smooth_tensor(window_size, phi8)
+phi10 = smooth_tensor(window_size, phi10)
+rho8 = smooth_tensor(window_size, rho8)
+phirho8 = smooth_tensor(window_size, phirho8)
+
+# loot_ppo = smooth_tensor(window_size, loot_ppo)
+axs[axis].plot(steps[:500], phi10[:500], color=colors[0])
+axs[axis].plot(steps[:500], phi8[:500], color=colors[1])
+axs[axis].plot(steps[:500], phi1[:500], color=colors[2])
+axs[axis].plot(steps[:500], rho8[:500], color=colors[3])
+axs[axis].plot(steps[:500], phirho8[:500], color=colors[4])
+
+lb_phi1, ub_phi1 = conf_interval(phi1[:500], colors[2])
+lb_phi8, ub_phi8 = conf_interval(phi8[:500], colors[1])
+lb_phi10, ub_phi10 = conf_interval(phi10[:500], colors[0])
+lb_rho8, ub_rho8 = conf_interval(rho8[:500], colors[3])
+lb_phirho8, ub_phirho8 = conf_interval(phirho8[:500], colors[4])
+
+axs[axis].fill_between(steps[:500], lb_phi1, ub_phi1, color=colors[2], alpha=0.3)
+axs[axis].fill_between(steps[:500], lb_phi8, ub_phi8, color=colors[1], alpha=0.3)
+axs[axis].fill_between(steps[:500], lb_phi10, ub_phi10, color=colors[0], alpha=0.3)
+axs[axis].fill_between(steps[:500], lb_rho8, ub_rho8, color=colors[3], alpha=0.3)
+axs[axis].fill_between(steps[:500], lb_phirho8, ub_phirho8, color=colors[4], alpha=0.3)
+
+# axs[1].fill_between(loot_steps[:200], lb_loot_ppo, ub_loot_ppo, color=colors[0], alpha=0.3)
+
+axs[axis].set_title('Threefish')
+
+formatter = ticker.FuncFormatter(thousands)
+plt.gca().xaxis.set_major_formatter(formatter)
 # threefish_phi1 = smooth_tensor(window_size, threefish_phi1)
 # threefish_phi8 = smooth_tensor(window_size, threefish_phi8)
 # threefish_ppo = smooth_tensor(window_size, threefish_ppo)
